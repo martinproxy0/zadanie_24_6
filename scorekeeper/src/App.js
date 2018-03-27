640b7e0fd2204b644c9b4d34e444b81b111531d7
martinproxy0 <marcinsornek@gmail.com>
2018-03-27 02:36:03 +0200

    Add code for 24_6

--- scorekeeper/src/App.js
+++ scorekeeper/src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PlayersList from './components/PlayersList/PlayersList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      players: [
+        {
+          name: 'Kinia',
+          score: 5,
+        },
+        {
+          name: 'Ali',
+          score: 0,
+        }
+      ]
+    }
+  }
+  
+  onPlayerRemove = (name) => {
+    this.setState({
+      players: this.state.players.filter((player) => (player.name !== name))
+    });
+  }
+
+  onPlayerAdd = (playerName) => {
+    const newPlayer = {
+      name: playerName,
+      score: 0,
+    }
+    this.setState({
+      players: [...this.state.players, newPlayer]
+    })
+  }
+
+  onScoreUpdate = (playerIndex, scoreChange) => {
+    this.setState({
+      players: this.state.players.map((player, index) => {
+        if (index === playerIndex) {
+          return { ...player, score: player.score + scoreChange };
+        }
+        return player;
+      })
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AddPlayer onPlayerAdd={this.onPlayerAdd} />
+        <PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate} onPlayerRemove={this.onPlayerRemove}/>
       </div>
     );
   }
--- scorekeeper/src/App.test.js
+++ scorekeeper/src/App.test.js
@@ -1,9 +1,55 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import PlayersList from './components/PlayersList/PlayersList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
+});
+
+it('should update player score', () => {
+  const appComponent = shallow(<App players={[]} />);
+  const players = [
+    {
+      name: 'Kunegunda',
+      score: 5,
+    }
+  ]
+  appComponent.setState({ players });
+
+  const onScoreUpdate = appComponent.find(PlayersList).prop('onScoreUpdate');
+
+  onScoreUpdate(0, -3);
+
+  const playersAfterUpdate = appComponent.state().players;
+
+  expect(playersAfterUpdate[0].score).toEqual(2);
+
+});
+
+it('should add player to state', () => {
+  const appComponent = shallow(<App players={[]} />);
+
+  const onPlayerAdd = appComponent.find(AddPlayer).prop('onPlayerAdd');
+  onPlayerAdd('Ania');
+
+
+  const players = appComponent.state().players;
+
+  expect(players[2].score).toEqual(0);
+  expect(players.length).toEqual(3);
+  expect(players[2].name).toEqual('Ania');
+});
+
+it('should delete player from state', () => {
+  const appComponent = shallow(<App players={[]} />);
+
+  const onPlayerRemove = appComponent.find(PlayersList).prop('onPlayerRemove');
+  onPlayerRemove('Ali');
+
+
+  const players = appComponent.state().players;
+
+  expect(players.length).toEqual(1);
 });
